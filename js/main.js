403ec5c65055f62aa6d0b661b38b2727c5c4256e
Matt Gelfand <matthew.gelfand@gmail.com>
2014-10-29 17:11:48 -0400

    joke

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*Fit Text*/
-$(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '70px' });
+$(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 
 
 $('a').smoothScroll();
