dafdd344076ecca3391102dd94a3a018c5e918fd
Matt Gelfand <matthew.gelfand@gmail.com>
2014-10-31 16:58:39 -0400

    sexy hamburger

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@ $(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 
 $('a').smoothScroll();
 
+document.querySelector( ".toggle-icon" ).addEventListener( "click", function() {
+  this.classList.toggle( "active" );
+});
