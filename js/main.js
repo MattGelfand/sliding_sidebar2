0ed9c82364ccc7cbcf883e7bab037df26207ef2e
MattGelfand <matthew.gelfand@gmail.com>
2014-11-02 22:54:57 -0500

    slopy

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ $(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 $('a').smoothScroll();
 
 
+$(function() {
+	$('.text-lrg').click(function() {
+		$('.expand > div').toggle(100)
+	});
+});
