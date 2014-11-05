4db1c1d0f2708e191e420ba56be78916940dd0d0
MattGelfand <matthew.gelfand@gmail.com>
2014-11-04 23:17:07 -0500

    remove skew and font changew

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 $('.extended').addClass('hide');
 
 $(function() {
-	$('.text-lrg').on("click", "a", function(event) {
+	$('.expand').on("click", "a", function(event) {
 		event.preventDefault();
 		$(this).parent().siblings('.extended').toggleClass('hide');
 	});
