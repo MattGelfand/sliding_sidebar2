9ffca126d319ab0b8906fb29560cf83a6235688d
MattGelfand <matthew.gelfand@gmail.com>
2014-11-07 16:11:43 -0500

    getting close needs cleaning

--- js/main.js
+++ js/main.js
@@ -15,8 +15,13 @@ $(function() {
 });
 */
 
-$(document).ready( function(){
-        $(".btn").click(function () {
-            $(".extra").show("slow");
-        });
-    });
+$(function() {      
+        $(".accordion section").css("opacity","1.0");     
+        $(".accordion section").hover(function () {     
+            $(this).siblings().stop().animate({opacity: 0.1}, "slow");   
+        },          
+        function () {      
+                $(".accordion section").stop().animate({opacity: 1.0}, "slow");       
+        });   
+});
+
