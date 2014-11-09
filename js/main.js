fd3b3345a18b50674bc23fb55384a01e47c58393
MattGelfand <matthew.gelfand@gmail.com>
2014-11-08 19:34:37 -0500

    just add modals

--- js/main.js
+++ js/main.js
@@ -5,16 +5,6 @@ $(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 $('a').smoothScroll();
 
 
-/*$('.extended').addClass('hide');
-
-$(function() {
-	$('.expand').on("click", "a", function(event) {
-		event.preventDefault();
-		$(this).parent().siblings('.extended').toggleClass("hide");
-	});
-});
-*/
-
 $(function() {      
         $(".accordion section").css("opacity","1.0");     
         $(".accordion section").hover(function () {     
