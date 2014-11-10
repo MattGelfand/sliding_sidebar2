9033c462ee8348975a9c57d46b5660698ac68d2a
MattGelfand <matthew.gelfand@gmail.com>
2014-11-10 00:00:12 -0500

    mess

--- js/main.js
+++ js/main.js
@@ -5,13 +5,11 @@ $(".words").fitText(1.2, { minFontSize: '20px', maxFontSize: '50px' });
 $('a').smoothScroll();
 
 
-$(function() {      
-        $(".accordion section").css("opacity","1.0");     
-        $(".accordion section").hover(function () {     
-            $(this).siblings().stop().animate({opacity: 0.1}, "slow");   
-        },          
-        function () {      
-                $(".accordion section").stop().animate({opacity: 1.0}, "slow");       
-        });   
-});
+
+/*$("superbutton").headroom({
+"offset": 205
+  });
+
+headroom.init();
+*/
 
